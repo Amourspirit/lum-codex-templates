3018ce3e28352f1467da390e375180c9cb7dbf6e
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2026-01-23 12:56:06 -0500

    refactor: Remove unused DESCOPE_CLIENT import from session.py

--- api/lib/descope/session.py
+++ api/lib/descope/session.py
@@ -4,7 +4,6 @@ from fastapi import HTTPException, Security, status
 # from fastapi.security import HTTPAuthorizationCredentials
 from ...lib.descope.auth import AUTH
 from ...models.descope.descope_session import DescopeSession
-from ..descope.client import DESCOPE_CLIENT
 
 
 def get_descope_session(
